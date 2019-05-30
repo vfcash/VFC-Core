c83223050bb69db352dc97d6790b80d6f17c4f44
vfcash <50428242+vfcash@users.noreply.github.com>
2019-05-30 01:13:29 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ clear
 clear
 rm coin
 sudo rm /usr/bin/coin
-gcc-8 -pthread base58.c crc64.c ecc.c sha3.c main.c -o coin
+gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -o coin
 sudo cp coin /usr/bin/coin
