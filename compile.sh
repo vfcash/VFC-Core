211ee035f8ef914621a3139634b71fc7c36c3c49
vfcash <50428242+vfcash@users.noreply.github.com>
2019-05-30 01:14:02 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,3 +4,6 @@ rm coin
 sudo rm /usr/bin/coin
 gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -o coin
 sudo cp coin /usr/bin/coin
+sudo chmod 0777 /usr/bin/coin
+sudo mkdir /var/log/vfc
+sudo chmod 0777 /var/log/vfc
