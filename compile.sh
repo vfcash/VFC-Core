5204e21de0f610e6a326d335db5efc3dbe190751
vfcash <50428242+vfcash@users.noreply.github.com>
2019-07-11 16:02:48 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,9 +2,7 @@ clear
 clear
 rm coin
 rm /usr/bin/coin
-gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -o coin
+gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -lm -o coin
 cp coin /usr/bin/coin
 chmod 0777 /usr/bin/coin
-mkdir /var/log/vfc
-chmod 0777 /var/log/vfc
 echo "Compiled and Installed /usr/bin/coin and /var/log/vfc ;)"
