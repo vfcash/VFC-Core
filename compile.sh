8db7f8247bdc1b73fe0d04cc350fddf46e6d4f79
vfcash <50428242+vfcash@users.noreply.github.com>
2019-07-12 23:49:27 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,4 +5,4 @@ rm /usr/bin/coin
 gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -lm -o coin
 cp coin /usr/bin/coin
 chmod 0777 /usr/bin/coin
-echo "Compiled and Installed /usr/bin/coin and /srv/.vfc or ~/.vfc ;)"
+echo "Compiled and Installed /usr/bin/coin and /srv/.vfc or ~/.vfc "
