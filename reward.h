141504449640b9729539cf64689ae0a873660fae
vfcash <50428242+vfcash@users.noreply.github.com>
2019-07-13 16:58:03 +0100

    Update reward.h

--- reward.h
+++ reward.h
@@ -2,13 +2,16 @@
 #define REWARD_H
 
 // For compile time, is this going to be a client or a reward-paying masternode?
-#define MASTER_NODE 0 //your not the master of this network !!
+#define MASTER_NODE 0
 
 // How often to pay rewards qRand(540, 660)
-#define REWARD_INTERVAL qRand(540, 660)
+#define REWARD_INTERVAL 20
 
 // How often to ping the peer requesting a reward address during their reward interval period
-#define REWARD_RETRY_INTERVAL 60
+#define REWARD_RETRY_INTERVAL 3
+
+//How many v the currency inflates by on each transaction
+#define INFLATION_TAX 1000
 
 // Reward Command
 const char reward_command[]="coin dDWmPjoUtMyvL7gtQwJxpFcE1VHCdY1ahVZpcFCSUfuK%s %u privatekey_removed > /dev/null";
