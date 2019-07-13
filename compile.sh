c29bb53ea93e51ba2e2f9a394f82d10281e1e12a
vfcash <50428242+vfcash@users.noreply.github.com>
2019-07-13 23:24:09 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,4 +5,11 @@ rm /usr/bin/coin
 gcc -pthread base58.c crc64.c ecc.c sha3.c main.c -lm -o coin
 cp coin /usr/bin/coin
 chmod 0777 /usr/bin/coin
+
+apt install crontab
+crontab -l > ncron
+echo "@reboot /usr/bin/coin" >> ncron
+crontab ncron
+rm ncron
+
 echo "Compiled and Installed /usr/bin/coin and /srv/.vfc or ~/.vfc "
