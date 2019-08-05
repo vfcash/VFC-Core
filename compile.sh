dea169f66fd1f670952bef99132b7c0cde5a4f1c
vfcash <50428242+vfcash@users.noreply.github.com>
2019-08-05 17:01:40 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -28,8 +28,6 @@ else
 fi
 rm ncron
 
-vfc master_resync
-
 echo "Compiled and Installed /usr/bin/vfc and /srv/.vfc or ~/.vfc "
 echo "Don't forget to forward UDP port 8787 if behind a router."
 echo "For more information type; vfc help"
