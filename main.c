c0560cc6f92e52b2dcea661c66a38039b381ae4e
James <James@voxdsp.com>
2019-08-19 19:41:10 +0100

    federated mining difficulty

--- main.c
+++ main.c
@@ -522,7 +522,7 @@ inline static double getMiningDifficulty()
     // const time_t lt = time(0);
     // const struct tm* tmi = gmtime(&lt);
     // return (double)(tmi->tm_hour+1) * 0.01; 
-    return 0.24;
+    return network_difficulty;
 }
 
 inline static uint64_t diff2val(const double ra)
