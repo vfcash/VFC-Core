115cb59f96b39a3ca8d2a60329759576fac086e8
James <James@voxdsp.com>
2019-08-23 15:51:22 +0100

    general

--- main.c
+++ main.c
@@ -3470,6 +3470,10 @@ int main(int argc , char *argv[])
                 {
                     //base58 priv key
                     char* bpriv = strtok(l, " ");
+                    if(bpriv == NULL)
+                        bpriv = strtok(l, "\n");
+                    if(bpriv == NULL)
+                        continue;
 
                     //priv as bytes
                     struct addr subg_priv;
@@ -3505,6 +3509,10 @@ int main(int argc , char *argv[])
                 {
                     //base58 priv key
                     char* bpriv = strtok(l, " ");
+                    if(bpriv == NULL)
+                        bpriv = strtok(l, "\n");
+                    if(bpriv == NULL)
+                        continue;
 
                     //priv as bytes
                     struct addr subg_priv;
