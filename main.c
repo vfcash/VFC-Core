cfb0e6cb38a8ccebbb34328b23fc96445bd03af9
James <James@voxdsp.com>
2019-08-24 21:54:10 +0100

    general

--- main.c
+++ main.c
@@ -759,7 +759,7 @@ uint isPeerAlive(const uint id)
 {
     const uint pd = time(0)-(peer_timeouts[id]-MAX_PEER_EXPIRE_SECONDS);
     const uint md = time(0) - peer_rm[id];
-    if(pd <= PING_INTERVAL*4 && md <= PING_INTERVAL*4)
+    if(pd <= PING_INTERVAL*4 && md <= PING_INTERVAL*8)
         return 1;
     return 0;
 }
