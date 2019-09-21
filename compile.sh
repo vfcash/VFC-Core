cb0718ba18c82b1cb178dd5e9e4167bc41682e84
James <James@voxdsp.com>
2019-09-21 21:51:51 +0100

    general

--- compile.sh
+++ compile.sh
@@ -9,8 +9,10 @@ clear
 pkill vfc
 rm vfc
 rm /usr/bin/vfc
+rm /usr/bin/vfui
 gcc -std=gnu99 -Ofast -pthread base58.c crc64.c ecc.c sha3.c main.c -lm -o vfc
 cp vfc /usr/bin/vfc
+cp bin/vfui-config /usr/bin/vfui
 chmod 0777 /usr/bin/vfc
 crontab -l > ncron
 if grep -qxF '*/6 * * * * /usr/bin/vfc' ncron; then
@@ -23,4 +25,4 @@ fi
 rm ncron
 echo "Compiled and Installed /usr/bin/vfc and /srv/.vfc or ~/.vfc "
 echo "Don't forget to forward UDP port 8787 if behind a router."
-echo "For more information type; vfc help"
+echo "For more information type; vfc help or vfui for the GUI"
