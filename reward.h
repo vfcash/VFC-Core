97a778cb217c65fcefb2145dbd41c39dd3b2f4bc
James <James@voxdsp.com>
2019-10-26 15:17:43 +0100

    general

--- reward.h
+++ reward.h
@@ -7,16 +7,7 @@
 //Run as root or not, recommended to run as root if your using this as a local service, for personal use, run as user (0)
 #define RUN_AS_ROOT 0
 
-// How often to pay rewards
-#define REWARD_INTERVAL 20
-
-// How often to ping the peer requesting a reward address during their reward interval period
-#define REWARD_RETRY_INTERVAL 3
-
 //How many v the currency inflates by on each transaction
 #define INFLATION_TAX 1000
-
-// Reward Command
-const char reward_command[]="vfc dDWmPjoUtMyvL7gtQwJxpFcE1VHCdY1ahVZpcFCSUfuK%s %.3f privatekey_removed > /dev/null";
 
 #endif
