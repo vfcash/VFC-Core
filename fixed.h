5607bf35d831e450bb34b46f1e26191b3a5b997c
vfcash <50428242+vfcash@users.noreply.github.com>
2019-11-30 15:33:17 +0000

    Update fixed.h

--- fixed.h
+++ fixed.h
@@ -4,10 +4,13 @@
 // For compile time, is this going to be a client or a reward-paying masternode?
 #define MASTER_NODE 0
 
-//Run as root or not, recommended to run as root if your using this as a local service, for personal use, run as user (0)
+// Run as root or not, recommended to run as root if your using this as a local service, for personal use, run as user (0)
 #define RUN_AS_ROOT 0
 
-//How many v the currency inflates by on each transaction
+// How many v the currency inflates by on each transaction
 #define INFLATION_TAX 3000
+
+// Easiest minting difficulty.
+#define MIN_DIFFICULTY 0.180
 
 #endif
