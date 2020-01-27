a0e4f9078e2bb6008bf78b9189ee4431ea52c7b7
vfcash <50428242+vfcash@users.noreply.github.com>
2020-01-27 16:10:56 +0000

    Update fixed.h

--- fixed.h
+++ fixed.h
@@ -1,7 +1,7 @@
 #ifndef FIXED_H
 #define FIXED_H
 
-// For compile time, is this going to be a client or a reward-paying masternode?
+// For compile time, is this going to be a client (0) or a web-service masternode (1)?
 #define MASTER_NODE 0
 
 // Run as root or not, recommended to run as root if your using this as a local service, for personal use, run as user (0)
