8cf97f135b409a16aa031f29578a26f2a92686ae
vfcash <50428242+vfcash@users.noreply.github.com>
2020-02-15 13:42:23 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2827,7 +2827,7 @@ void *generalThread(void *arg)
         {
             peersBroadcast(mid, 8);
             peersBroadcast("a", 1); //Give us your user-agent too please
-            broadcastUserAgent(); //Forcibly tell everyone our user-agent
+            //broadcastUserAgent(); //Forcibly tell everyone our user-agent
             peer_timeouts[0] = time(0) + MAX_PEER_EXPIRE_SECONDS; //Reset master timeout
             pr = time(0) + PING_INTERVAL;
         }
