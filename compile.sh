78b0788acdd60d5c02b8b4dbf470542eaa3096fb
vfcash <50428242+vfcash@users.noreply.github.com>
2021-07-04 14:12:44 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,23 +8,19 @@ apt install --assume-yes xterm
 apt install --assume-yes qt5-default
 apt install --assume-yes libqt5widgets5
 apt install --assume-yes libsdl2-2.0-0
-apt install --assume-yes libsdl2-net-2.0-0
 apt install --assume-yes libomp-9-dev
 clear
 clear
 pkill vfc
 rm bin/vfc
 rm /usr/bin/vfc
-rm /usr/bin/vfui
 rm /usr/bin/vfwallet
 rm /usr/bin/cminer
 gcc -std=gnu99 -Ofast -pthread base58.c crc64.c ecc.c sha3.c main.c -lm -o bin/vfc
 cp bin/vfc /usr/bin/vfc
-cp bin/vfui /usr/bin/vfui
 cp bin/vfwallet /usr/bin/vfwallet
 cp bin/cminer /usr/bin/cminer
 chmod +x /usr/bin/vfc
-chmod +x /usr/bin/vfui
 chmod +x /usr/bin/vfwallet
 chmod +x /usr/bin/cminer
 crontab -l > ncron
