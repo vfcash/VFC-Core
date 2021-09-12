ec6ae8cd862f16cc7fcca2bd0cc8d29cc15bf113
vfcash <50428242+vfcash@users.noreply.github.com>
2021-09-12 10:40:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3072,13 +3072,9 @@ uint isNodeRunning()
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(31963); // could just use gport here
     
-    if(bind(s, (struct sockaddr*)&server, sizeof(server)) == 0)
-    {
-        close(s);
+    if(bind(s, (struct sockaddr*)&server, sizeof(server)) < 0)
         return 0; //Bind success, process not already running.
-    }
 
-    close(s);
     return 1; //Say it's running
 }
 
